e16ef0ab12118faf0a092ab334daeac340c13884
RCyro <r.cyron95@gmail.com>
2022-11-27 15:32:21 +0100

    hibernateUtil

--- src/main/java/pl/sda/zadanie/produkt/ProduktMain.java
+++ src/main/java/pl/sda/zadanie/produkt/ProduktMain.java
@@ -1,4 +1,27 @@
 package pl.sda.zadanie.produkt;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
 public class ProduktMain {
+    @Data
+    @Entity
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public class Osoba {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+        @Column(nullable = false)
+        private String nazwa;                                                    // 'imie'                   VARCHAR(255)
+        private String kategoria;                                       // 'kierunek_nauczania'     VARCHAR(255)
+        private String indeks;
+        private LocalDate dataUrodzenia;
 }
